4245363975625cacadf60b42694dfa4a6983967e
Magson Chostak <mchostak@chaplau.com>
2021-05-05 17:19:34 -0300

    implemented absolute imports and integrate react-router

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 const PromotionCard = ({ promotion }) => (
     <div className="promotionCard">
-        <img src={promotion.imageUrl} className="promotionCard__image"/>
+        <img src={promotion.imageUrl}  alt='' className="promotionCard__image"/>
         <div className="promotionCard__info">
             <h1 className="promotionCard__title">{promotion.title}</h1>
             <span className="promotionCard__price">R$ {promotion.price}</span>
@@ -13,8 +13,17 @@ const PromotionCard = ({ promotion }) => (
                         "{promotion.comments[0].comment}"
                     </div>
                 )}
-                <div className="promotionCard__comments-count">{promotion.comments.length} {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}</div>
-                <a href={promotion.url} target="_blank" className="promotionCard__link">IR PARA O SITE</a>
+                <div className="promotionCard__comments-count">
+                    {promotion.comments.length} {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}
+                    </div>
+                <a 
+                    href={promotion.url} 
+                    target="_blank"
+                    rel="noopener noreferrer"         
+                    className="promotionCard__link"
+                >
+                    IR PARA O SITE
+                </a>
             </footer>
         </div>
     </div>
